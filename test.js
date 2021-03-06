dce45afe820336bfa648a6914b30e809fc928bd9
zaid bin saeed <47746351+zaid13@users.noreply.github.com>
2021-03-06 17:32:28 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 const http = require('http');
 const express = require("express");
-const socketIo = require("socket.io");
+// const socketIo = require("socket.io");
 
 // App setup
 const PORT = process.env.PORT || 8050;
@@ -17,45 +17,3 @@ app.get('/', (req, res) => {
 
 const server = http.Server(app)
 server.listen(PORT)
-
-
-const io = socketIo(server);
-
-io.on("connection", function (socket) {
-
-  console.log(" connection request");
-
-  socket.emit('hello',{
-    greeting:'hello zaid'
-  })
-  console.log("Made socket connection");
-});
-
-
-// var WebSocket = require('ws');
-
-// var port = process.env.PORT || 3000;
-
-// var serverio = new WebSocket.Server(
-//   {
-//     port: port,
-//   }
-// )
-
-// let msg = "Server: Welcome!";
-
-// serverio.on('connection', function connection(client) {
-//   client.send(msg);
-//   client.on('message', function incoming(message) {
-//     msg = message;
-//     for(var cl of serverio.clients) {
-//       cl.send(message);
-//     }
-//     console.log("Received the following message:\n" + message);
-//   });
-// });
-
-
-
-
-// Socket setup
