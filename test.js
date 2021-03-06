d1c609636b615e5e31babdb10053b974bf8cfb57
zaid bin saeed <47746351+zaid13@users.noreply.github.com>
2021-03-06 17:37:57 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,34 @@
 
-const http = require('http');
-const express = require("express");
-// const socketIo = require("socket.io");
+// const http = require('http');
+// const express = require("express");
+// // const socketIo = require("socket.io");
 
-// App setup
-const PORT = process.env.PORT || 8050;
-const app = express();
+// // App setup
+// const PORT = process.env.PORT || 8050;
+// const app = express();
 
 
 
+
+// app.get('/', (req, res) => {
+//   res.send('Hello socket io/!')
+// })
+
+
+// // const server = http.Server(app)
+// // server.listen(PORT)
+
+// const server = app.listen(PORT, function () {
+//   console.log(`Listening on port ${PORT}`);
+//   console.log(`http://localhost:${PORT}`);
+// });
+
+
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello socket io/!')
+  res.send('Hi!')
 })
 
-
-// const server = http.Server(app)
-// server.listen(PORT)
-
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
+app.listen(3000, () => console.log('Server ready'))
