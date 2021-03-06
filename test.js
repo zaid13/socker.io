ec6ff44bcbdd1dc3ca7b3e9390b8c224dabae46e
zaid13 <xaidsaeed@gmail.com>
2021-03-06 07:33:18 +0500

    heydf

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 // App setup
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
